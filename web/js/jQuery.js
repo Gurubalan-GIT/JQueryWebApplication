901d14a781d6498cd8f9c8fe53a2a0568c7a7d6c
Gurubalan Harikrishnan <gurubalan.work@gmail.com>
2018-06-25 10:04:27 +0530

    Pushed Changes

--- web/js/jQuery.js
+++ web/js/jQuery.js
@@ -1,14 +1,4 @@
-/*$(document).ready(function (){
-    $('#id-button1').click(function (){
-        var x=$('#id-fn').val();
-        $.ajax({
-            url:'Servlet',
-            type:'post',
-            data:{fn:x},
-            success: function (data){
-                $('#id-ajaxresponse').text(data);
-            }
-        })
-    })
+$(document).ready(function (){
+
+
 });
-*/
